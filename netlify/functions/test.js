9cc0c2dd551e5f80043f5a70c97480adda4d18a1
Ashok <ashok@N14545.local>
2024-04-20 20:37:32 +0530

    serverless files added

---
+++ netlify/functions/test.js
@@ -0,0 +1,8 @@
+// hello.js
+
+exports.handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello from the serverless function!" })
+    };
+};
