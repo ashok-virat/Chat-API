947cbb2b7097284d0038ec6a07ac177a0a642c73
Ashok <ashok@N14545.local>
2024-04-20 22:02:32 +0530

    netlify changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,6 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
 const express = require('express');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
-const router = express.Router();
 
 const app = express();
 
@@ -12,6 +9,4 @@ app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => res.send("Hello World!"));
 
-// app.use(router);
-
-exports.handler = serverless(app);
+module.exports.handler = serverless(app);
